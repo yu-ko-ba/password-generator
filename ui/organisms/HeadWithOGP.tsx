c3148bd6d4f1c054f426de3395adfcb0e86904e6
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-05-28 16:12:00 +0900

    OGPの確認用

--- ui/organisms/HeadWithOGP.tsx
+++ ui/organisms/HeadWithOGP.tsx
@@ -1,19 +1,23 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
+import { env } from "process"
 
 type Props = {
   title: string
 }
 
 export default function HeadWithOGP({ title }: Props) {
-  const r = useRouter()
-  console.log(r)
+  const router = useRouter()
+  const baseUrl = env.NODE_ENV === "production" ? "https://password-generator-nine-blond.vercel.app" : "http://localhost:3000"
   return (
     <Head>
       <title>{title}</title>
       <meta name="theme-color" content="#bbdefb" />
       <meta property="og:title" content={title} />
-      <meta property="og:url" content={r.domainLocales?.join("")} />
+      <meta property="og:url" content={baseUrl + router.asPath} />
+      <meta property="og:image" content="https://raw.githubusercontent.com/yu-ko-ba/password-generator/main/public/favicon.ico" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="@yu_ko_ba" />
     </Head>
   )
 }
